9b7aa46cda567dfd3f4c98d62b9932e3d48a8972
Gautam <gautam@grappus.com>
2018-12-30 17:35:06 +0530

    Finish the menu items

--- app/index.js
+++ app/index.js
@@ -1,14 +1,9 @@
 var React = require("react");
 var ReactDOM = require("react-dom");
 require("./index.css");
+var App = require("./components/App");
 // state
 // lifecycle
 //  UI
-
-class App extends React.Component {
-  render() {
-    return <div>Hello to the github battle!!</div>;
-  }
-}
 
 ReactDOM.render(<App />, document.getElementById("app"));
